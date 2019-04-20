12d435de0033ec019798178b17d4c0bcdc917a92
wrm_glb <939367545@qq.com>
2019-04-21 00:31:33 +0800

    update User

--- src/main/java/com/sghy1801/entity/User.java
+++ src/main/java/com/sghy1801/entity/User.java
@@ -5,4 +5,66 @@ package com.sghy1801.entity;
  * @create 2019-04-21 0:10
  */
 public class User {
+    //用户id
+    private int id;
+    //用户名
+    private String username;
+    //用户密码
+    private String passwrod;
+    //用户手机号
+    private  String phone;
+    //用户邮箱
+    private String email;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswrod() {
+        return passwrod;
+    }
+
+    public void setPasswrod(String passwrod) {
+        this.passwrod = passwrod;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", passwrod='" + passwrod + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
