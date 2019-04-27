e4f809f3a046d9b7174b387505bc9c2ef3a26005
wzih <48348775+wzih@users.noreply.github.com>
2019-04-27 18:31:37 +0800

    wzh
    json数组

--- src/main/java/com/sghy1801/controller/TemperatrueController.java
+++ src/main/java/com/sghy1801/controller/TemperatrueController.java
@@ -1,17 +1,18 @@
 package com.sghy1801.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.sghy1801.entity.Temperature;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.sghy1801.service.TemperatureService;
-import com.sghy1801.util.AllUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
+import javax.json.Json;
 import java.util.*;
 
 
@@ -22,6 +23,14 @@ public class TemperatrueController {
     @Autowired
     private TemperatureService service;
 
+    /**
+     * 逐小时的温度
+     * @param machineID
+     * @param oldtime
+     * @param newtime
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/getHoursTemperature",method = RequestMethod.POST)
     public @ResponseBody
     String getHoursTemperature(String machineID,String oldtime,String newtime) throws Exception {
@@ -41,13 +50,48 @@ public class TemperatrueController {
 
         //获取温度列表
         List<Map> temperatures = service.getHoursTemperature(map);
-        //转换成json格式
-        String j = JSON.toJSONString(temperatures);
+        JSONObject jsonObject = new JSONObject();
+        String day = "";
+        //所需数组个数
+        int count = temperatures.size()%24==0?temperatures.size()/24:temperatures.size()/24+1;
+        for (int i = 0; i < count; i++) {
+            if (day==""){
+                day = temperatures.get(i).get("hours").toString().substring(8,13);
+                System.out.println(day);
+            }else {
+                if (!day.equals(temperatures.get(i).get("hours").toString().substring(8,13))){
 
-        return j;
+                }
+            }
+            jsonObject.put("hours",temperatures.get(i).get("hours"));
+
+            Object[] arr = new Object[24];
+            int size =temperatures.size();
+            for (int j = 0; j <= size ; j++) {
+                if (temperatures.get(j).get("hoursavg")==null){
+                    arr[j] = null;
+                }
+                arr[j]= temperatures.get(j).get("hoursavg");
+                jsonObject.put("hoursavg",arr);
+            }
+        }
+
+
+        //转换成json格式
+
+        String j = JSON.toJSONString(jsonObject);
+        System.out.println(j);
+        return "";
     }
 
-
+    /**
+     * 逐天的温度
+     * @param machineID
+     * @param oldtime
+     * @param newtime
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/getDaysTemperature",method = RequestMethod.POST)
     public @ResponseBody
     String getDaysTemperature(String machineID,String oldtime,String newtime) throws Exception {
@@ -67,6 +111,7 @@ public class TemperatrueController {
 
         //获取温度列表
         List<Map> temperatures = service.getDaysTemperature(map);
+
         //转换成json格式
         String j = JSON.toJSONString(temperatures);
 
