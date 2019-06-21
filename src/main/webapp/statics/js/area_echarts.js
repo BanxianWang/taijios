74c31cee98082384429d12e712aec16d92a9fa25
wzih <1025237482@qq.com>
2019-06-21 15:08:58 +0800

    后台地图数据绑定版

--- src/main/webapp/statics/js/area_echarts.js
+++ src/main/webapp/statics/js/area_echarts.js
@@ -1,87 +1,100 @@
-setTimeout(aa(), 100);
 
-function aa() {
-    //数据纯属虚构
 
-    var data = [{
+//ajax获取各城市用户数量
+$.ajax({
+        url: "/jsp/getLocalDistribution",
+        type: "post",
+        dataType: "json",
+        success: function (msg) {
+            aa(msg.all);
+        }
+
+})
+
+
+function aa(msg) {
+
+    var max = 0;
+    //包含23个城市的数组
+    var data =  [{
         name: '江苏',
-        value: 5000
+        value: 0
     },
         {
             name: '天津',
-            value: 1000
+            value: 0
         },
         {
             name: '北京',
-            value: 3000
+            value: 0
         },
         {
             name: '上海',
-            value: 2000
+            value: 0
         },
         {
             name: '河北',
-            value: 2000
+            value: 0
         },
         {
             name: '河南',
-            value: 3000
+            value: 0
         },
         {
             name: '云南',
-            value: 1000
+            value: 0
         },
         {
             name: '湖南',
-            value: 2000
+            value: 0
         },
         {
             name: '安徽',
-            value: 3000
+            value: 0
         },
         {
             name: '山东',
-            value: 3500
+            value: 0
         },
         {
             name: '浙江',
-            value: 3000
+            value: 0
         },
         {
             name: '江西',
-            value: 2000
+            value: 0
         },
         {
             name: '湖北',
-            value: 2000
+            value: 0
         },
         {
             name: '福建',
-            value: 2000
+            value: 0
         },
         {
             name: '贵州',
-            value: 1200
+            value: 0
         },
         {
             name: '广东',
-            value: 2500
+            value: 0
         },
         {
             name: '四川',
-            value: 1000
+            value: 0
         },
         {
             name: '台湾',
-            value: 500
+            value: 0
         },
         {
             name: '香港',
-            value: 200
+            value: 0
         },
         {
             name: '澳门',
-            value: 500
+            value: 0
         },
 
 
@@ -128,12 +141,6 @@ function aa() {
             name: '吉林',
             value: 0
         },
-
-        {
-            name: '贵州',
-            value: 0
-        },
-
         {
             name: '青海',
             value: 0
@@ -153,24 +160,38 @@ function aa() {
         },
         {
             name: '重庆',
-            value: 500
+            value: 0
         }
 
     ];
 
-    var option = {
 
+    //将得到的数据循环放入data数组
+    for(var i = 0;i<23;i++){
+        for(var j = 0;j<msg.length;j++){
+            //当查的数据城市与data名相同时，将值放入
+            if(data[i].name==msg[j].city){
+                data[i].value = msg[j].num;
+
+                //获取最多用户城市的用户数
+                if(data[i].value>max){
+                    max = data[i].value;
+                }
+            }
+        }
+
+    }
+
+    var option = {
+        //鼠标悬浮时显示的数据
         tooltip: {
             show: true,
             formatter: function (params) {
-                if (params.data['value'] != undefined) {
-                    return params.name + '：' + params.data['value'] + '人'
-                } else {
-                    return params.name + '：0 人'
-
-                }
+                return params.name + '：' + params.data['value'] + '人'
             },
         },
+
+
         visualMap: {
             type: 'continuous',
             orient: 'horizontal',
@@ -180,7 +201,7 @@ function aa() {
             showLabel: true,
             seriesIndex: [0],
             min: 0,
-            max: 5000,
+            max: max,
             inRange: {
                 color: ['#6FCF6A', '#FFFD64', '#FF5000']
             },
@@ -191,7 +212,7 @@ function aa() {
             bottom: 30
         },
         geo: {
-            // roam: true,
+            //roam: true,
             map: 'china',
             left: '270',
             right:'270'
@@ -223,6 +244,7 @@ function aa() {
     myChart.setOption(option);
 
 
+    //适应窗口比例
     window.addEventListener("resize", function () {
         myChart.resize();
     });
